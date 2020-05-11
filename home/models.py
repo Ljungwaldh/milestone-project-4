dec486f289e01a8e6afc933609bc4353d33f0620
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-05-11 14:40:34 +0000

    Put together first draft of all the models to be used in the project, all placed in models.py under the home app for the time being. This is being done to have an initial and clear presentation of proposed models and how they would link with one another. Will look to review these, add changes where needed, followed by placing each model within relevant apps

--- home/models.py
+++ home/models.py
@@ -1,3 +1,73 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils import timezone
 
 # Create your models here.
+
+
+class SubscriptionPlan (models.Model):
+    name = models.CharField(max_length=254)
+    reward = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    tier = models.ForeignKey(Tier, null=False, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+
+class Tier (models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class User (models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    username = models.CharField(max_length=50)
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    is_creator = models.Boolean(default=False, null=True, blank=True)
+
+    def __str__(self):
+        return self.username
+
+
+class GameProject (models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    owner = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(blank=False, null=False, default=timezone.now)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment (models.Model):
+    content = models.TextField()
+    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(blank=False, null=False, default=timezone.now)
+    blog_article = models.ForeignKey(BlogArticle, null=False, blank=False, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user
+
+
+class BlogArticle (models.Model):
+    blog_title = models.CharField(max_length=50)
+    blog_content = models.TextField()
+    game_project = models.ForeignKey(GameProject, null=False, blank=False, on_delete=models.CASCADE)
+    tiers = models.ForeignKey(Tiers, null=False, blank=False, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(blank=False, null=False, default=timezone.now)
+
+    def __str__(self):
+        return self.blog_title
+
+
+class Order (models.Model):
+    user = models.ForeignKey(User, null=False, blank=False, on_delete=models.CASCADE)
+    subscription_plan = models.ForeignKey(SubscriptionPlan, null=False, blank=False, on_delete=models.CASCADE)
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+
+    def __str__(self):
+        return self.order_number
--- subscriptions/models.py
+++ subscriptions/models.py
@@ -1,12 +1,3 @@
 from django.db import models
 
 # Create your models here.
-
-
-class Subscriptions (models.Model):
-    name = models.Charfield(max_length=254)
-    reward = models.TextField()
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-
-    def __str__(self):
-        return self.name
