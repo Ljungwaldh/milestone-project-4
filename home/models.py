d0a4acb8ae27aa391b3a3df8f404a5ff4390d61b
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-05-12 08:04:49 +0000

    Made minor adjustments to models placed currently in the models.py file in the home app

--- home/models.py
+++ home/models.py
@@ -5,8 +5,15 @@ from django.utils import timezone
 # Create your models here.
 
 
+class Tier (models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
 class SubscriptionPlan (models.Model):
-    name = models.CharField(max_length=254)
+    title = models.CharField(max_length=50)
     reward = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
     tier = models.ForeignKey(Tier, null=False, blank=False, on_delete=models.CASCADE)
@@ -15,19 +22,12 @@ class SubscriptionPlan (models.Model):
         return self.name
 
 
-class Tier (models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
-
-
 class Profile (models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    is_creator = models.Boolean(default=False, null=True, blank=True)
+    is_creator = models.BooleanField(default=False, null=True, blank=True)
 
     def __str__(self):
-        return self.user
+        return self.user.username
 
 
 class GameProject (models.Model):
@@ -40,6 +40,17 @@ class GameProject (models.Model):
         return self.title
 
 
+class BlogArticle (models.Model):
+    blog_title = models.CharField(max_length=50)
+    blog_content = models.TextField()
+    game_project = models.ForeignKey(GameProject, null=False, blank=False, on_delete=models.CASCADE)
+    tiers = models.ForeignKey(Tier, null=False, blank=False, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(blank=False, null=False, default=timezone.now)
+
+    def __str__(self):
+        return self.blog_title
+
+
 class Comment (models.Model):
     content = models.TextField()
     user = models.ForeignKey(Profile, null=False, blank=False, on_delete=models.CASCADE)
@@ -47,24 +58,14 @@ class Comment (models.Model):
     blog_article = models.ForeignKey(BlogArticle, null=False, blank=False, on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.user
-
-
-class BlogArticle (models.Model):
-    blog_title = models.CharField(max_length=50)
-    blog_content = models.TextField()
-    game_project = models.ForeignKey(GameProject, null=False, blank=False, on_delete=models.CASCADE)
-    tiers = models.ForeignKey(Tiers, null=False, blank=False, on_delete=models.CASCADE)
-    created_at = models.DateTimeField(blank=False, null=False, default=timezone.now)
-
-    def __str__(self):
-        return self.blog_title
+        return self.user.username
 
 
 class Order (models.Model):
     user = models.ForeignKey(Profile, null=False, blank=False, on_delete=models.CASCADE)
     subscription_plan = models.ForeignKey(SubscriptionPlan, null=False, blank=False, on_delete=models.CASCADE)
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
 
     def __str__(self):
         return self.order_number
