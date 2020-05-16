cefc105dec2fca76bf8505136bee261a181cb3f1
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-05-16 16:38:33 +0000

    Added order number generation and saving functions to models.py in checkout app (credits go to Chris Zielinski's Boutique Ado project for code). Also created GET request functionality to views.py, will subsequently look to code POST request functioanlity

--- checkout/models.py
+++ checkout/models.py
@@ -1,20 +1,10 @@
+import uuid
 from django.db import models
-from subscription.models import SubscriptionPlan
+
 from profiles.models import Profile
 from subscription.models import Donation
 from django.utils import timezone
 from gameproject.models import GameProject
-
-
-# class Order(models.Model):
-#     user = models.ForeignKey(Profile, null=True, blank=True, on_delete=models.SET_NULL, related_name='orders')
-#     subscription_plan = models.ForeignKey(SubscriptionPlan, null=False, blank=False, on_delete=models.CASCADE)
-#     order_number = models.CharField(max_length=32, null=False, editable=False)
-#     stripe_pid = models.CharField(max_length=254, null=False, blank=False, default='')
-#     created_at = models.DateTimeField(blank=False, null=False, default=timezone.now)
-
-#     def __str__(self):
-#         return self.order_number
 
 
 class Order(models.Model):
@@ -25,6 +15,22 @@ class Order(models.Model):
     created_at = models.DateTimeField(blank=False, null=False, default=timezone.now)
     game_project = models.ForeignKey(GameProject, null=True, blank=True, on_delete=models.CASCADE)
 
+    def _generate_order_number(self):
+        """
+        Generate a random, unique order number using UUID.
+        Credits to Boutique Ado project by Chris Zielinski.
+        """
+        return uuid.uuid4().hex.upper()
+
+    def save(self, *args, **kwargs):
+        """
+        Override the original save method to set the order number
+        if it hasn't been set already.
+        Credits to Boutique Ado project by Chris Zielinski.
+        """
+        if not self.order_number:
+            self.order_number = self._generate_order_number()
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.stripe_pid
--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404
+from gameproject.models import GameProject
+from subscription.models import Donation
 
 
 def donate(request):
@@ -6,9 +8,13 @@ def donate(request):
     print(request.GET.get('game_project'))
     donation_type = request.GET.get('donation_type')
     game_project = request.GET.get('game_project')
+    project = get_object_or_404(GameProject, pk=game_project)
+    donation = get_object_or_404(Donation, pk=donation_type)
 
     template = 'checkout/checkout.html'
     context = {
+        'donation': donation,
+        'project': project,
     }
 
     return render(request, template, context)
