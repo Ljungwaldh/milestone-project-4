32a51ab34d530e32d76c19205b57ee1df4d9ec0d
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-05-18 14:52:22 +0000

    fixed test_views to pass the test using the status code 200. This required the test creating and logging in a mock-user first before it went through the test, followed by changing the suyntax of asserting the template used

--- profiles/test_views.py
+++ profiles/test_views.py
@@ -1,9 +1,16 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 
 class TestCustomSignupFrom(TestCase):
 
     def test_get_profile_page(self):
-        response = self.client.get('/')
+
+        User.objects.create(username='temporary',
+                            email='temp@temp.com',
+                            password='secret')
+
+        self.client.login(username='temporary', password='secret')
+        response = self.client.get('/profile/', follow=True)
         self.assertEqual(response.status_code, 200)
-        self.assertTemplateUsed(response, 'profiles/profile.html')
+        self.assertTemplateUsed(template_name='profiles/profile.html')
