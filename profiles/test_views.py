49b5628837f847c41191b83b884448a671389f1a
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-05-18 14:24:46 +0000

    started to write test for views in profiles app, need to fix a failure related to http status code being 301 when it should be 200

--- profiles/test_views.py
+++ profiles/test_views.py
@@ -1,1 +1,9 @@
 from django.test import TestCase
+
+
+class TestCustomSignupFrom(TestCase):
+
+    def test_get_profile_page(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'profiles/profile.html')
