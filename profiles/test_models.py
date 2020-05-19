bce7d2bc3835a840532c337810926a8fa40608d4
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-05-19 14:26:02 +0000

    Written tests that pass for models in profile app

--- profiles/test_models.py
+++ profiles/test_models.py
@@ -1,12 +1,15 @@
 from django.test import TestCase
 from .models import Profile
+from django.contrib.auth.models import User
 
 
-class TestCustomSignupFrom(TestCase):
+class TestModel(TestCase):
     def test_iscreator_defaults_to_false(self):
-        profile = Profile.objects.create(email='brian@brian.com', username='thebrian', password1='hellothistest', password2='hellothistest')
+        user = User.objects.create(email='brian@brian.com', username='thebrian', password='hellothistest')
+        profile = Profile.objects.create(user=user)
         self.assertFalse(profile.is_creator)
 
     def test_string_is_equal_to_username(self):
-        profile = Profile.objects.get(pk=1)
-        self.assertEqual(str(profile), profile.username)
+        user = User.objects.create(email='brian@brian.com', username='thebrian', password='hellothistest')
+        profile = Profile.objects.create(user=user)
+        self.assertEqual(str(profile), profile.user.username)
