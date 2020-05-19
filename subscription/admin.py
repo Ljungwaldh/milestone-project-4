b2e7a729aa9c685e2c43996988743e2f349ca385
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-05-19 18:14:52 +0000

    Completed cleaning up of database and reorganisation of apps and models to better fit the current idea being implemented for this project

--- subscription/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from .models import SubscriptionPlan, Tier, Donation_Old
-
-admin.site.register(SubscriptionPlan)
-admin.site.register(Tier)
-admin.site.register(Donation_Old)
