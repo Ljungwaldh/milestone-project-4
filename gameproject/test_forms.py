3d7eb0a496c1fa2ecc0b971fdcb65b92616abd5c
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-05-21 06:39:56 +0000

    Written tests for testing forms in gameprojects app

--- gameproject/test_forms.py
+++ gameproject/test_forms.py
@@ -19,18 +19,16 @@ class TestProjectForm(TestCase):
         self.assertIn('description', form.errors.keys())
         self.assertEqual(form.errors['description'][0], 'This field is required.')
 
-    # def test_valid_form_creates_game_project(self):
-    #     user = User.objects.create(username='testuser')
-    #     user.set_password('try12345')
-    #     user.save()
-    #     profile = Profile.objects.create(user=user, is_creator=True)
-    #     self.client.login(username='testuser', password='try12345')
+    def test_valid_form_leads_to_correct_redirect(self):
+        user = User.objects.create(username='testuser')
+        user.set_password('try12345')
+        user.save()
+        profile = Profile.objects.create(user=user, is_creator=True)
+        logged_in = self.client.login(username='testuser', password='try12345')
 
-    #     form = ProjectForm({'title': 'test', 'description': 'testing'})
-    #     post_data = {'title': 'test', 'description': 'testing'}
+        form = ProjectForm({'title': 'test', 'description': 'testing'})
+        post_data = {'title': 'test', 'description': 'testing'}
 
-    #     response = self.client.post('add_project', post_data)
-    #     self.assertTrue(form.is_valid())
-    #     self.assertEqual(response.status_code, 301)
-    #     print(GameProject.objects.all())
-    #     game_project = GameProject.objects.get(title='test')
+        response = self.client.post('/gameproject/add_project', post_data)
+        self.assertTrue(form.is_valid())
+        self.assertEqual(response.status_code, 302)
