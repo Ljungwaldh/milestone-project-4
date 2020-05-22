d8fd544d499ef468edbf1623c379a1830a1282a5
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-05-22 13:29:20 +0000

    testing rendering media file in heroku

--- gamestarter/settings.py
+++ gamestarter/settings.py
@@ -134,7 +134,7 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-    # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
