0b372bfb3d81ea999b47e0fe31f985b6f6ff2ec3
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-05-31 16:51:05 +0000

    Samll styling adjustments to checkout.css. Found that checkout.css wasn't linked to checkout.html - with it now linked, the css and javascript are working. Attempted to iterate on messages syntax in base.html, however, not all message types are showing up for all view events with messages

--- checkout/views.py
+++ checkout/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404, reverse, redirect, HttpResponse
+from django.shortcuts import render, get_object_or_404, reverse, redirect
 from django.contrib import messages
 from django.conf import settings
 from django.contrib.auth.decorators import login_required
