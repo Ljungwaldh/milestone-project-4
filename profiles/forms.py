9ba6f0527f129749d201b303721cf0f83cb9debb
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-05-31 19:33:31 +0000

    Removed redundant imports from profiles app

--- profiles/forms.py
+++ profiles/forms.py
@@ -2,7 +2,6 @@ from django import forms
 from .models import Profile
 from django.contrib.auth.models import User
 from allauth.account.forms import SignupForm
-from django.db import models
 
 
 class UserForm(forms.ModelForm):
@@ -14,11 +13,9 @@ class UserForm(forms.ModelForm):
 
 class CustomSignupForm(SignupForm):
 
-    # is_creator = forms.BooleanField(label='is creator', required=False)
-
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.fields['is_creator'] = forms.BooleanField(label='Sign up as a Creator', required=False)
+        self.fields['is_creator'] = forms.BooleanField(label='Signup as a Creator', required=False)
 
     def save(self, request):
         is_creator = self.cleaned_data.pop('is_creator')
