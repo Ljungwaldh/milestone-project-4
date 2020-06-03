f6de64fee3e76c80aa1302a552acdbc181e58e40
Ljungwaldh <gustaf.ljungwaldh@live.co.uk>
2020-06-03 15:48:29 +0000

    Adjusted urls files in gamestarter folder to try and solve media file issue in deployed version

--- gamestarter/urls.py
+++ gamestarter/urls.py
@@ -25,11 +25,10 @@ urlpatterns = [
     path('profile/', include('profiles.urls')),
     path('gameproject/', include('gameproject.urls')),
     path('checkout/', include('checkout.urls')),
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
-if settings.DEBUG:
-    urlpatterns += static(
-        settings.MEDIA_URL,
-        document_root=settings.MEDIA_ROOT
-        )
+urlpatterns += static(
+    settings.MEDIA_URL,
+    document_root=settings.MEDIA_ROOT
+    )
